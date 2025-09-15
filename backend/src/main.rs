171acf7b0663d2d16a7e2668d81bd8a6595e6475
Lorhan <gabriellorhan0@gmail.com>
2025-09-15 10:37:08 -0300

    deploy bug ajust

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -27,7 +27,7 @@ async fn main() {
         );
 
     // endereço localhost:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("Servidor rodando em http://{}", addr);
 
     // Cria o listener TCP
